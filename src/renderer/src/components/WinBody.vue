9deb8b05da1f8a4cf580ffb6a32bb3febd5f3803
scucat <scucat@sgitee.com>
2026-03-14 10:40:54 +0800

    Initial commit

---
+++ src/renderer/src/components/WinBody.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="win-body">
+    <p>{{ props.text }}</p>
+  </div>
+</template>
+<script lang="ts" setup>
+import { } from 'vue'
+
+const props = defineProps<{
+  text: string
+}>()
+</script>
+<style lang="less" scoped></style>
---
+++ src/renderer/src/main.ts
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import '@dongls/xwindow/dist/index.css'
+import { xWindow } from '@dongls/xwindow'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(xWindow)
+app.mount('#app')
+// createApp(App).mount('#app')
